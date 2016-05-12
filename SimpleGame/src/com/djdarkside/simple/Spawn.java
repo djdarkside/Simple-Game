97e5689994c3f07d1c4de39b373c39cd1e3a7122
djdarkside <mitchellbaptist@gmail.com>
2016-05-12 16:24:20 -0700

    Various Changes to Spawn

--- SimpleGame/src/com/djdarkside/simple/Spawn.java
+++ SimpleGame/src/com/djdarkside/simple/Spawn.java
@@ -17,7 +17,7 @@ public class Spawn {
 	
 	public void update() {
 		scoreKeep++;		
-		if (scoreKeep >= 250) {
+		if (scoreKeep >= 200) {
 			scoreKeep = 0;
 			
 			hud.setLevel(hud.getLevel() + 1);	
@@ -25,14 +25,18 @@ public class Spawn {
 			if (hud.getLevel() == 2) {
 				coin = new Coin(r.nextInt(Game.getWindowWidth()),r.nextInt(Game.getWindowHeight()),ID.Coin,handler);
 				handler.addObject(coin);
-				//handler.addObject(new EnemyBoss(r.nextInt(Game.getWindowWidth() - 50), r.nextInt(Game.getWindowHeight() - 50), ID.EnemyBoss, handler));
 			} else if (hud.getLevel() == 3) {
 				handler.removeObject(coin);
+				coin = new Coin(r.nextInt(Game.getWindowWidth()),r.nextInt(Game.getWindowHeight()),ID.Coin,handler);
+				handler.addObject(coin);
 				handler.addObject(new BasicEnemy(r.nextInt(Game.getWindowWidth()- 50), r.nextInt(Game.getWindowHeight()- 50), ID.BasicEnemy, handler));
 			} else if (hud.getLevel() == 4) {
+				handler.removeObject(coin);
 				handler.addObject(new FastEnemy(r.nextInt(Game.getWindowWidth()- 50), r.nextInt(Game.getWindowHeight()- 50), ID.FastEnemy, handler));
 			} else if (hud.getLevel() == 5) {
 				handler.addObject(new SmartEnemy(r.nextInt(Game.getWindowWidth()- 50), r.nextInt(Game.getWindowHeight()- 50), ID.SmartEnemy, handler));
+			} else if (hud.getLevel() == 6) {
+				handler.addObject(new EnemyBoss(Game.getWindowWidth() / 2, -65, ID.EnemyBoss, handler));
 			}
 		}
 	}	
